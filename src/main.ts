dbf16ea1a310a12b40d106be9a3a386692c658eb
Rafay Zia <rafayzia10@gmail.com>
2023-08-10 22:35:24 +0500

    updated package.json

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { setupSwagger } from 'swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = new ConfigService();
-  const PORT = configService.get('PORT') || 5000;
   const apiPrefix = 'api';
   app.setGlobalPrefix(apiPrefix);
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
-  await app.listen(PORT, () =>
-    Logger.log(`App successfully running on port ${PORT}.`),
+  await app.listen(process.env.PORT || 5000, () =>
+    Logger.log(`App successfully running on port ${process.env.PORT || 5000}.`),
   );
 }
 bootstrap();
